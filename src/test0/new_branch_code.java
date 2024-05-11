b33d812dfb2cf75a1552243bae44cb11ec6dc381
Manga <amangaqa@gmail.com>
2024-05-11 14:19:16 +0100

    new branch code

---
+++ src/test0/new_branch_code.java
@@ -0,0 +1,12 @@
+package test0;
+
+public class new_branch_code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("new branch created");
+
+	}
+
+}
